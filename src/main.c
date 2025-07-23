bd4add3856f625eef75282cebfd9adb9d7dc7642
sumde2 <jason.radatus@gmail.com>
2025-07-23 07:39:11 -0400

    Update and rename main.c to src/main.c

--- src/main.c
+++ src/main.c
@@ -6,10 +6,35 @@
 
 // VIEWERS NOTE: I kinda have trouble with not spamming conditionals everywhere :P
 
-int bottle_levels[2][4][4] = {{{0,0,0,0},
+int bottle_levels[7][4][4] = {{{0,0,0,0},
+                            {0,0,1,1},
+                            {0,0,1,1},
+                            {0,0,0,0}},
+
+                            {{0,2,2,2},
+                            {0,0,1,2},
+                            {0,0,1,1},
+                            {0,0,0,1}},
+
+                            {{0,3,3,3},
+                            {2,2,1,2},
+                            {0,0,1,1},
+                            {0,1,2,3}},
+
+                            {{0,0,0,0},
                             {1,1,2,3},
                             {1,2,3,2},
                             {3,3,2,1}},
+
+                            {{0,2,2,3},
+                            {2,1,3,2},
+                            {0,0,3,3},
+                            {0,1,1,1}},
+
+                            {{0,0,0,0},
+                            {2,1,2,3},
+                            {3,3,2,3},
+                            {1,1,1,2}},
 
                             {{0,0,1,1},
                             {0,0,2,3},
@@ -22,9 +47,9 @@ int highlighted_bottle = 0;
 int selected_bottle = -1;
 
 int current_bottles[4][4] = {{0,0,0,0},
-                            {1,1,2,3},
-                            {1,2,3,2},
-                            {3,3,2,1}}; // The current bottle levels that can be changed at any time.
+                            {0,0,1,1},
+                            {0,0,1,1},
+                            {0,0,0,0}}; // The current bottle levels that can be changed at any time.
 
 int bottle_colors[4] = {0x94, 0xE0, 0x07, 0x18}; // Colour of each level.
 
@@ -36,11 +61,20 @@ void draw() {
     gfx_FillScreen(0xFF);
 
     gfx_SetTextTransparentColor(0);
-    gfx_SetTextBGColor(255);
-    gfx_SetTextFGColor(127);
+    gfx_SetTextBGColor(0xFF);
+    gfx_SetTextFGColor(0x7F);
     gfx_SetTextScale(3,3);
     gfx_SetTextXY(42,20);
     gfx_PrintString("Liquid Sort!");
+
+    gfx_SetTextFGColor(0x98);
+    gfx_SetTextScale(2,2);
+    gfx_SetTextXY(32,200);
+	gfx_PrintString("Current Level: ");
+    gfx_SetTextXY(16*sizeof("Current Level: "),200);
+	gfx_PrintInt(current_level, 1);
+
+    int win_count = 0;
 
     for (int i = 0; i<4; i++) {
         gfx_SetColor(0x94); // Grayish colour.
@@ -57,9 +91,28 @@ void draw() {
         for (int j = 0; j<4; j++) {
             gfx_SetColor(bottle_colors[current_bottles[i][j]]);
             gfx_FillRectangle((i+1)*64-12, 68+(j*28), 26, 28);
+            if (bottle_colors[current_bottles[i][j]]==bottle_colors[current_bottles[i][0]]) {
+                win_count++;
+            }
         }
     }
+
+    if (win_count==16) {
+        gfx_SetTextFGColor(0xE0);
+        gfx_SetTextScale(4,4);
+        gfx_SetTextXY(52,104);
+        gfx_PrintString("You won!");
+    }
     gfx_SwapDraw();
+    if (win_count==16) {
+        delay(2000);
+        current_level++;
+        for (int i = 0; i<4; i++) {
+            for (int j = 0; j<4; j++) {
+                current_bottles[i][j] = bottle_levels[current_level][i][j];
+            }
+        }
+    }
 }
 
 int main(void) {
